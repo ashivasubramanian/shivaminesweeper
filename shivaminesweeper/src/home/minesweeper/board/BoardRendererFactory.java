32b256750706f0c685c975fa6ddd9aab61718365
ashivasubramanian <ashivasubramanian@9f6d3a52-63cd-11de-81b3-7dd168245822>
2009-10-09 16:10:48 +0000

    The BoardRenderer class must be a singleton. This class ensures that only one instance of BoardRenderer exists.

---
+++ shivaminesweeper/src/home/minesweeper/board/BoardRendererFactory.java
@@ -0,0 +1,36 @@
+package home.minesweeper.board;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+/** 
+ * Creates one (and only one) instance of the 
+ * <code>BoardRenderer</code> class and ensures that
+ * clients receive only that instance.
+ * TODO must be initialized in a static initialization block.
+ */
+final public class BoardRendererFactory {
+
+		
+			
+			
+				
+				
+				private BoardRendererFactory(){
+				/*
+				 * Does nothing.
+				 */ }
+
+				/**
+				 * @uml.property  name="INSTANCE" readOnly="true"
+				 */
+				private static BoardRenderer INSTANCE;
+
+					
+						
+						
+						public static BoardRenderer returnBoardRendererAsSingleton(){
+						
+												return null;
+											 }
+
+}
