7c98b6c0191ee55f34e1d17909233ec323cf29d5
ashivasubramanian <ashivasubramanian@9f6d3a52-63cd-11de-81b3-7dd168245822>
2010-03-03 18:20:13 +0000

    Removed test methods that do not belong here.

--- shivaminesweeper/tests/home/minesweeper/board/BoardTest.java
+++ shivaminesweeper/tests/home/minesweeper/board/BoardTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BoardTest {
-
+	
 	/**
 	 * The board generated in beginner mode shall have 9 rows by 9 columns.
 	 */
@@ -18,22 +18,6 @@ public class BoardTest {
 	}
 	
 	/**
-	 * The board generated in beginner mode shall have at the max 10 mines.
-	 */
-	@Test
-	public void beginnerModeShallHaveNoMoreThan10Mines() {
-		Board board = new Board(BoardModes.BEGINNER);
-		int mineCount = 0 ;
-		int rows[][] = board.getRows();
-		for (int i = 0; i < rows.length; i++) {
-			for (int j = 0; j < rows[i].length; j++) {
-				if (rows[i][j] == -1) mineCount++;
-			}
-		}
-		assertEquals("Mine count is not 10", 10, mineCount);
-	}
-
-	/**
 	 * The board generated in intermediate mode shall have 16 rows by 16 columns.
 	 */
 	@Test
@@ -43,22 +27,6 @@ public class BoardTest {
 		assertEquals("Rows does not equal 16.", rows, 16);
 		int columns = board.getRows()[0].length;
 		assertEquals("Columns does not equal 16", columns, 16);
-	}
-
-	/**
-	 * The board generated in intermediate mode shall have at the max 40 mines.
-	 */
-	@Test
-	public void intermediateModeShallHaveNoMoreThan40Mines() {
-		Board board = new Board(BoardModes.INTERMEDIATE);
-		int mineCount = 0 ;
-		int rows[][] = board.getRows();
-		for (int i = 0; i < rows.length; i++) {
-			for (int j = 0; j < rows[i].length; j++) {
-				if (rows[i][j] == -1) mineCount++;
-			}
-		}
-		assertEquals("Mine count is not 40", mineCount, 40);
 	}
 
 	/**
@@ -74,31 +42,15 @@ public class BoardTest {
 	}
 
 	/**
-	 * The board generated in advanced mode shall have at the max 99 mines.
-	 */
-	@Test
-	public void advancedModeShallHaveNoMoreThan99Mines() {
-		Board board = new Board(BoardModes.ADVANCED);
-		int mineCount = 0 ;
-		int rows[][] = board.getRows();
-		for (int i = 0; i < rows.length; i++) {
-			for (int j = 0; j < rows[i].length; j++) {
-				if (rows[i][j] == -1) mineCount++;
-			}
-		}
-		assertEquals("Mine count is not 99", mineCount, 99);
-	}
-	
-	/**
 	 * Marking a tile should add it to the marked tiles list.
 	 */
 	@Test
 	public void markingATileShouldAddToTheMarkedTilesList() {
 		Board board = new Board(BoardModes.BEGINNER);
 		int markedTilesCount = board.getMarkedTiles().size();
-		board.markTile("2", "2");
+		board.markTile(2, 2, 4);
 		assertEquals("Tile not added to marked list.",
-			board.getMarkedTiles().size(), markedTilesCount + 1);
+			markedTilesCount + 1, board.getMarkedTiles().size());
 	}
 
 	/**
@@ -107,10 +59,10 @@ public class BoardTest {
 	@Test
 	public void unmarkingATileShouldRemoveItFromTheMarkedTilesList() {
 		Board board = new Board(BoardModes.BEGINNER);
-		board.markTile("2", "2");
+		board.markTile(2, 2, 5);
 		int markedTilesCount = board.getMarkedTiles().size();
-		board.unmarkTile("2", "2");
-		assertEquals("Tile not added to marked list.",
-			board.getMarkedTiles().size(), markedTilesCount - 1);
+		board.unmarkTile(2, 2);
+		assertEquals("Tile not removed from marked list.",
+			markedTilesCount - 1, board.getMarkedTiles().size());
 	}
 }
