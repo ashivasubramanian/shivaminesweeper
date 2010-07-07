5a2586738be1bf965bdbd6cdd203099667915cfd
ashivasubramanian <ashivasubramanian@9f6d3a52-63cd-11de-81b3-7dd168245822>
2010-07-07 01:58:45 +0000

    Added validations and Javadoc

--- shivaminesweeper/src/home/minesweeper/board/BoardModes.java
+++ shivaminesweeper/src/home/minesweeper/board/BoardModes.java
@@ -12,24 +12,70 @@ public enum BoardModes {
 	INTERMEDIATE(16, 16, 40),
 	ADVANCED(16, 30, 99);
 
+	
+	/**
+	 * Stores the no of rows in the board.
+	 */
 	private int rows;
+	
+	/**
+	 * Stores the no of columns in the board.
+	 */
 	private int columns;
+	
+	/**
+	 * Stores the no of mines on the board.
+	 */
 	private int mineCount;
 	
+	/**
+	 * Constructor that initializes the mode.
+	 * 
+	 * @param rows the no of rows in the board
+	 * @param columns the no of columns in the board
+	 * @param mineCount the no of mines on the board
+	 */
 	BoardModes(int rows, int columns, int mineCount) {
+		if (rows < 1) {
+			throw new IllegalArgumentException(
+				"Rows parameter cannot be less than 1.");
+		}
+		if (columns < 1) {
+			throw new IllegalArgumentException(
+				"Columns parameter cannot be less than 1.");
+		}
+		if (mineCount < 1) {
+			throw new IllegalArgumentException(
+				"Mine count parameter cannot be less than 1.");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		this.mineCount = mineCount;
 	}
 	
+	/**
+	 * Returns the no of rows in the board.
+	 * 
+	 * @return the row count
+	 */
 	public int getRows() {
 		return this.rows;
 	}
 	
+	/**
+	 * Returns the no of columns in the board.
+	 * 
+	 * @return the column count
+	 */
 	public int getColumns() {
 		return this.columns;
 	}
 	
+	/**
+	 * Returns the no of mines on the board.
+	 * 
+	 * @return the mine count
+	 */
 	public int getMineCount() {
 		return this.mineCount;
 	}
