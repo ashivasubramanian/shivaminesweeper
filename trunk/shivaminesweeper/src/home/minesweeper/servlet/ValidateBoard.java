0d724a37a0f89b69afb0c87ef24537f5f6720413
ashivasubramanian <ashivasubramanian@9f6d3a52-63cd-11de-81b3-7dd168245822>
2010-11-30 06:54:28 +0000

    Forgot to checkin this file for validating boards. Part of revision 111

---
+++ trunk/shivaminesweeper/src/home/minesweeper/servlet/ValidateBoard.java
@@ -0,0 +1,46 @@
+package home.minesweeper.servlet;
+
+import home.minesweeper.board.Board;
+
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ValidateBoard implements ServletRequestAware {
+	
+	private HttpServletRequest request;
+	
+	private InputStream inputStream;
+	
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	public String execute() {
+		logger.entering(this.getClass().getName(), "execute");
+		Board board = (Board) request.getSession().getAttribute(request.getRequestedSessionId());
+		String json = "";
+		if (board.validateCurrentBoardState()) {
+			json = "{\"validity\" : true}";
+		} else {
+			json = "{\"validity\" : false}";
+		}
+		inputStream = new StringBufferInputStream(json);
+		logger.info(json);
+		logger.exiting(this.getClass().getName(), "execute");
+		return ActionSupport.SUCCESS;
+	}
+	
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	public InputStream getInputStream() {
+		return this.inputStream;
+	}
+
+}
